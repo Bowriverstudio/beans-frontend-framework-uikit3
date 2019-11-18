07f0c7dab3cd4d8fd71b02df4634640a7dcb633f
Maurice Tadros <maurice.tadros@gmail.com>
2019-11-17 17:00:43 -0700

    Formated search form widget

--- lib/templates/fragments/searchform.php
+++ lib/templates/fragments/searchform.php
@@ -5,6 +5,7 @@
  * @package Beans\Framework\Templates\Fragments
  *
  * @since   1.0.0
+ * @updated 2.0.0 - changed support to uikit3.
  */
 
 // Filter.
@@ -17,41 +18,53 @@ beans_add_smart_action( 'get_search_form', 'beans_search_form' );
  * @return string The form.
  */
 function beans_search_form() {
-	$output = beans_open_markup(
-		'beans_search_form',
-		'form',
-		array(
-			'class'  => 'uk-form-todo uk-form-icon-todo uk-form-icon-flip-todo uk-width-1-1',
-			'method' => 'get',
-			'action' => esc_url( home_url( '/' ) ),
-			'role'   => 'search',
-		)
-	);
 
-		$output .= beans_selfclose_markup(
-			'beans_search_form_input',
-			'input',
-			array(
-				'class'       => 'uk-width-1-1',
-				'type'        => 'search',
-				'placeholder' => __( 'Search', 'tm-beans' ), // Automatically escaped.
-				'value'       => esc_attr( get_search_query() ),
-				'name'        => 's',
-			)
-		);
+    $output = beans_open_markup(
+        'beans_search_form_wrapper',
+        'div',
+        array(
+            'class'  => 'uk-margin',
+        )
+    );
+        $output .= beans_open_markup(
+            'beans_search_form',
+            'form',
+            array(
+                'class'  => 'uk-search uk-search-default',
+                'method' => 'get',
+                'action' => esc_url( home_url( '/' ) ),
+                'role'   => 'search',
+            )
+        );
 
-		$output .= beans_open_markup(
-			'beans_search_form_input_icon',
-			'span',
-			array(
-				'aria-hidden' => 'true',
-                'uk-icon'     => 'icon:search'
-			)
-		);
 
-		$output .= beans_close_markup( 'beans_search_form_input_icon', 'span' );
+            $output .= beans_open_markup(
+                'beans_search_form_input_icon',
+                'span',
+                array(
+                    'class'       => 'uk-search-icon-flip uk-search-icon',
+                    'aria-hidden' => 'true',
+                    'uk-icon'     => 'icon:search'
+                )
+            );
 
-	$output .= beans_close_markup( 'beans_search_form', 'form' );
 
-	return $output;
+            $output .= beans_close_markup( 'beans_search_form_input_icon', 'span' );
+            $output .= beans_selfclose_markup(
+                'beans_search_form_input',
+                'input',
+                array(
+                    'class'       => 'uk-search-input uk-width-1-1',
+                    'type'        => 'search',
+                    'placeholder' => __( 'Search...', 'tm-beans' ), // Automatically escaped.
+                    'value'       => esc_attr( get_search_query() ),
+                    'name'        => 's',
+                )
+            );
+
+        $output .= beans_close_markup( 'beans_search_form', 'form' );
+
+    $output .= beans_close_markup( 'beans_search_form_wrapper', 'div' );
+
+    return $output;
 }
